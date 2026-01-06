2ef724634efefdf05e92a0760ae460670c84636f
user\Mickey <mickey11566@gmail.com>
2026-01-06 11:24:56 +0800

    feat: new framework with PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Swal from 'sweetalert2'
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,7 +15,10 @@ export class AppComponent {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    Swal.fire("SweetAlert2 is working!");
+    // Swal.fire("SweetAlert2 is working!");
 
+  }
+  test() {
+    Swal.fire("SweetAlert2 is working!");
   }
 }
